35fa6b5cbf21559faf571774c204baa11ec59cc3
IvanRep <ivanparra.develop@gmail.com>
2023-12-19 09:53:32 +0100

    Add SVG Animations
    - Add tree animation.
    
    - Add gift animation.

--- src/app/components/svg/gift/gift.component.ts
+++ src/app/components/svg/gift/gift.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gift',
   standalone: true,
   imports: [],
   templateUrl: './gift.component.svg',
-  styleUrl: './gift.component.scss'
+  styleUrl: './gift.component.scss',
 })
 export class GiftComponent {
-
+  @HostBinding('style.--box-duration')
+  @Input()
+  boxDuration = '3s';
+  @HostBinding('style.--bow-delay')
+  @Input()
+  bowDelay = this.boxDuration.split('s')[0] + 's';
+  @HostBinding('style.--bow-duration')
+  @Input()
+  bowDuration = '2s';
+  @HostBinding('style.--fill-duration')
+  @Input()
+  fillDuration = '2s';
 }
