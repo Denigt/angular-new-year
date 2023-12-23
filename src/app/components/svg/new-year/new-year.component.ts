3f99cad040e720e85e8debbcb44f77fd688117de
Denigt <guerrero.raulparra@gmail.com>
2023-12-23 22:43:08 +0100

    Add new year message svg

---
+++ src/app/components/svg/new-year/new-year.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-year',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-year.component.svg',
+  styleUrl: './new-year.component.scss'
+})
+export class NewYearComponent {
+
+}
