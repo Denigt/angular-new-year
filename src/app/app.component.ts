45dd347b498a222b8e8802667422e508bf5339e6
Denigt <guerrero.raulparra@gmail.com>
2023-12-26 02:01:04 +0100

    Remove unnecessary router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core'
 import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common'
-import { ActivatedRoute, RouterOutlet } from '@angular/router'
 import { MessageComponent } from './components/message/message.component'
 import { TreeComponent } from './components/svg/tree/tree.component'
 import { GiftComponent } from './components/svg/gift/gift.component'
@@ -11,7 +10,7 @@ import { GreetingService } from './services/greeting.service'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MessageComponent, TreeComponent, GiftComponent, NewYearComponent],
+  imports: [CommonModule, MessageComponent, TreeComponent, GiftComponent, NewYearComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -20,23 +19,20 @@ export class AppComponent implements OnInit {
   private code: string
 
   constructor (
-    public route: ActivatedRoute, 
     private confettiSv: ConfettiService,
     private greetingSv: GreetingService,
     @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit (): void {
-    if (isPlatformBrowser(this.platformId))
+    if (isPlatformBrowser(this.platformId)) {
       this.confettiSv.snowAnimationFrame();
+      this.code = new URLSearchParams(window.location.search).get('code') || undefined
+    }
 
     if (isPlatformServer(this.platformId))
       this.greetingSv.list().forEach(element => {
         console.log(`${element.name} -- ${element.code}`)
       })
-
-    this.route.queryParamMap.subscribe((params) => {
-      this.code = params.get('code')
-    })
   }
 
   get name (): string {
